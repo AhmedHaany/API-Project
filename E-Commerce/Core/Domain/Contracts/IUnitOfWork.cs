e298dbe8108f930426921cf9a5d1f73b82b53e7c
AhmedHaany <shmed2001@gmail.com>
2024-09-28 02:51:16 +0300

    Unit OF Work

---
+++ E-Commerce/Core/Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+	public interface IUnitOfWork
+	{
+		public Task<int> SaveChangesAsunc();
+		IGenericRepository <TEntity,TKey> GetGenericRepository<TEntity,TKey>() where TEntity : BaseEntity<TKey> ;
+	}
+}
