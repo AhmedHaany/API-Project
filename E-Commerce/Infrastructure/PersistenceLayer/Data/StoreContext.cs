850300f67a12f86f4c00eb9951def7f488dd298b
AhmedHaany <shmed2001@gmail.com>
2024-09-27 20:19:20 +0300

    Restful API

---
+++ E-Commerce/Infrastructure/PersistenceLayer/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistenceLayer.Data
+{
+	public class StoreContext : DbContext
+	{
+		public DbSet<Product> Products { get; set; }
+		public DbSet<ProductBrand> ProductBrands { get; set; }
+		public DbSet<ProductType> ProductTypes { get; set; }
+
+	}
+}
