fd0e01af772aed4900a292f69fd23c7246cd19eb
AhmedHaany <shmed2001@gmail.com>
2024-10-06 21:53:07 +0300

    Specifications

---
+++ E-Commerce/Core/Services/Specifications/ProductWithBrandAndSpecifications.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Contracts;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Specifications
+{
+	internal class ProductWithBrandAndSpecifications :Specifications<Product>
+	{
+		public ProductWithBrandAndSpecifications(int id):base(product=> product.Id==id )
+		{
+			AddInclude(product=>product.ProductBrand);
+			AddInclude(product=>product.ProductType);
+		}
+
+		public ProductWithBrandAndSpecifications() : base(null)
+		{
+			AddInclude(product => product.ProductBrand);
+			AddInclude(product => product.ProductType);
+		}
+
+
+	}
+}
